c2b1ee489db90cf14af54a508df0acf8859ead3a
polvora-fosforo <contaparacodigo999@gmail.com>
2021-01-26 19:39:45 -0300

    [this website]
    -> added this website page

--- components/ThisWebsite.jsx
+++ components/ThisWebsite.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import globalStyles from '../styles/global.module.css';
+import styles from '../styles/this-website.module.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ThisWebsite = () => {
-  return (<h1>This website</h1>);
+  return (
+    <section id="thisWebsite" className={`${globalStyles['container--fluid']} ${styles.background}`}>
+      <h2 className={`${globalStyles.subjectTitle} ${globalStyles['subjectTitle--alternate']}`}>This website</h2>
+
+      <div className={`${globalStyles.container} ${styles.repo}`}>
+        <p>This website was built using the framework Next.js and stylized using css modules. You can check the repository for this at:</p>
+        <a className={styles.repo__link} href="https://github.com/polvora-fosforo/arthurlara-institutional" target="_blank">
+          https://github.com/polvora-fosforo/arthurlara-institutional
+          <span className={styles['repo__link--external']}>
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </span>
+        </a>
+      </div>
+
+    </section>
+  );
 }
 
 export default ThisWebsite;
